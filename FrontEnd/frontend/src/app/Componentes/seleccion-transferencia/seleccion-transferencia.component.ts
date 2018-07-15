27ed476d78a80d0e763f7bab22a4bbca3c5b618c
mayliz95 <mayra.rosero@epn.edu.ec>
2018-07-15 01:12:48 -0500

    Seleccion de transferencia

--- FrontEnd/frontend/src/app/Componentes/seleccion-transferencia/seleccion-transferencia.component.ts
+++ FrontEnd/frontend/src/app/Componentes/seleccion-transferencia/seleccion-transferencia.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeleccionTransferenciaComponent implements OnInit {
 
+  listaIngredientes = [1,2,3,4,5,6,7,8];
   constructor() { }
 
   ngOnInit() {
