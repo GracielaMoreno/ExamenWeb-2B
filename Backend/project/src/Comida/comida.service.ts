8848926194b847f12a289ae180297670b8a32080
mayliz95 <mayra.rosero@epn.edu.ec>
2018-07-16 15:42:36 -0500

    carga de datos con relaciones

--- Backend/project/src/Comida/comida.service.ts
+++ Backend/project/src/Comida/comida.service.ts
@@ -2,6 +2,7 @@ import { Injectable} from "@nestjs/common";
 import {InjectRepository} from "@nestjs/typeorm";
 import {ComidaEntity} from "./comida.entity";
 import {Like, Repository} from "typeorm";
+import {UsuarioEntity} from "../Usuario/usuario.entity";
 
 
 @Injectable()
@@ -10,12 +11,14 @@ export class ComidaService {
     /*arregloComidas: ComidaClass[] = [new ComidaClass(1,'Salchi', 'Papas y salchica', 'Ecuador', 2,true)];*/
 
     listaComidas = [
-        {'id': 1, 'nombrePlato': 'Salchi', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg'},
-        {'id': 2, 'nombrePlato': 'Tacos', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg'},
-        {'id': 3, 'nombrePlato': 'Pastel Chocolate', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg'},
-        {'id': 4, 'nombrePlato': 'Pizza', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg'},
-        {'id': 5, 'nombrePlato': 'Alitas BBQ', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg'},
-        {'id': 6, 'nombrePlato': 'Pollo Jardinero', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg'}
+        {'id': 1, 'nombrePlato': 'Salchi', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg', 'usuarioId': 8},
+        {'id': 2, 'nombrePlato': 'Tacos', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg', 'usuarioId': 6},
+        {'id': 3, 'nombrePlato': 'Pastel Chocolate', 'descripcionPlato': 'Delicioso pastel con chocolate', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg', 'usuarioId': 1},
+        {'id': 4, 'nombrePlato': 'Pizza', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg', 'usuarioId': 3},
+        {'id': 5, 'nombrePlato': 'Alitas BBQ', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg', 'usuarioId': 5},
+        {'id': 6, 'nombrePlato': 'Pollo Jardinero', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 2, 'picante': true, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg', 'usuarioId': 2},
+        { 'id': 7, 'nombrePlato': 'Torta de Naranja', 'descripcionPlato': 'Papas y salchicha', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 5, 'picante': false, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg', 'usuarioId': 4},
+        { 'id': 8, 'nombrePlato': 'Arroz relleno', 'descripcionPlato': 'Arroz con pollo, arvejas y pimiento', 'nacionalidad': 'Ecuatoriana', 'numeroPersonas': 5, 'picante': false, 'urlImg': 'https://d25rq8gxcq0p71.cloudfront.net/language-guide/758/pepperoni%20pizza.jpg', 'usuarioId': 7}
     ];
 
     constructor(@InjectRepository(ComidaEntity)
@@ -32,7 +35,9 @@ export class ComidaService {
             comida.numeroPersonas =this.listaComidas[comidas].numeroPersonas;
             comida.picante = this.listaComidas[comidas].picante;
             comida.urlImg =  this.listaComidas[comidas].urlImg;
-            //ingrediente.comidaId = this.listaIngredientes[ingredientes].comidaId;
+            const usuario = new UsuarioEntity();
+            usuario.id = this.listaComidas[comidas].usuarioId;
+            comida.usuarioId = usuario;
             this.comidaRepository.save(comida);
         }
         return this.comidaRepository.find();
--- Backend/project/src/ingrediente/ingrediente.service.ts
+++ Backend/project/src/ingrediente/ingrediente.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@nestjs/common";
 import {InjectRepository} from "@nestjs/typeorm";
 import {IngredienteEntity} from "./ingrediente.entity";
 import {Like, Repository} from "typeorm";
+import {ComidaEntity} from "../Comida/comida.entity";
 
 @Injectable()
 export class IngredienteService {
@@ -9,18 +10,18 @@ export class IngredienteService {
     /*arregloIngredientes: IngredienteClass[] = [
         new IngredienteClass(1, 'papas', 2, 'Papas fritas', false, 'Nose', false, 1)];*/
     listaIngredientes = [
-        {'id': 1, 'nombreIngrediente': 'papas', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 2, 'nombreIngrediente': 'sal', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 3, 'nombreIngrediente': 'aceite', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 4, 'nombreIngrediente': 'pimienta', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 5, 'nombreIngrediente': 'harina', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 6, 'nombreIngrediente': 'arroz', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 7, 'nombreIngrediente': 'avena', 'cantidad': 3.2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 8, 'nombreIngrediente': 'leche', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 9, 'nombreIngrediente': 'lenteja', 'cantidad': 4.5, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 10, 'nombreIngrediente': 'maíz', 'cantidad': 8, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 11, 'nombreIngrediente': 'frejol', 'cantidad': 7, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'},
-        {'id': 12, 'nombreIngrediente': 'azucar', 'cantidad': 8, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg'}
+        {'id': 1, 'nombreIngrediente': 'papas', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 1},
+        {'id': 2, 'nombreIngrediente': 'sal', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 2},
+        {'id': 3, 'nombreIngrediente': 'aceite', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 4},
+        {'id': 4, 'nombreIngrediente': 'pimienta', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 5},
+        {'id': 5, 'nombreIngrediente': 'harina', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 3},
+        {'id': 6, 'nombreIngrediente': 'arroz', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 8},
+        {'id': 7, 'nombreIngrediente': 'avena', 'cantidad': 3.2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 7},
+        {'id': 8, 'nombreIngrediente': 'leche', 'cantidad': 2, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 7},
+        {'id': 9, 'nombreIngrediente': 'lenteja', 'cantidad': 4.5, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 6},
+        {'id': 10, 'nombreIngrediente': 'maíz', 'cantidad': 8, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 8},
+        {'id': 11, 'nombreIngrediente': 'frejol', 'cantidad': 7, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 8},
+        {'id': 12, 'nombreIngrediente': 'azucar', 'cantidad': 8, 'descripcionPreparacion': 'sfdsfdf', 'opcional': false, 'tipoIngrediente': 'nose', 'necesitaRefrigeracion': false, 'urlImg': 'https://image.freepik.com/foto-gratis/fondo-de-madera-con-marco-hecho-de-ingredientes_23-2147606551.jpg', 'comidaId': 3},
     ];
 
     constructor(@InjectRepository(IngredienteEntity)
@@ -38,7 +39,9 @@ export class IngredienteService {
             ingrediente.tipoIngrdiente = this.listaIngredientes[ingredientes].tipoIngrediente;
             ingrediente.necesitaRefrigeracion =  this.listaIngredientes[ingredientes].necesitaRefrigeracion;
             ingrediente.urlImg = this.listaIngredientes[ingredientes].urlImg;
-            //ingrediente.comidaId = this.listaIngredientes[ingredientes].comidaId;
+            const comida = new ComidaEntity();
+            comida.id = this.listaIngredientes[ingredientes].comidaId;
+            ingrediente.comidaId = comida;
             this.ingredienteRepository.save(ingrediente);
         }
         return this.ingredienteRepository.find();
